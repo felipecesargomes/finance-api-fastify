2860a56f64769486d4dc0ae640b2c0e842e5ac86
Felipe César <felipe.calmeida@luizalabs.com>
2025-02-07 22:09:56 -0300

    refactor: add field sessionId

--- db/migrations/20250202152822_create-transactions.ts
+++ db/migrations/20250202152822_create-transactions.ts
@@ -1,16 +1,15 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('transactions', (table) => {
         table.uuid('id').primary();
         table.string('title').notNullable();
         table.decimal('amount', 10, 2).notNullable();
         table.enum('type', ['credit', 'debit']).notNullable();
+        table.uuid('sessionId').notNullable();
         table.dateTime('createdAt').defaultTo(knex.fn.now()).notNullable();
     });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
     return knex.schema.dropTable('transactions');
